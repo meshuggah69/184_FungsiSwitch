2728471242b4788f7bf7c543187bd097df9fc48c
athar <athar@THUGSHAKERZ>
2024-03-26 11:04:00 +0700

    menambah input data penjumlahan

--- Projek kedua/Projek kedua.cpp
+++ Projek kedua/Projek kedua.cpp
@@ -33,5 +33,52 @@ void inputdata()
 
 int main()
 {
-   int 
+    int pilihan;
+    do
+    {
+        system("CLS");
+        cout << "Menu kalkulator sederhana" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukan pilihan";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            inputdata();
+            cout << "\nHasil penjumlahan: " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 2:
+            inputdata();
+            cout << "\nHasil pengurangan: " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 3:
+            inputdata();
+            cout << "\nHasil perkalian: " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 4:
+            inputdata();
+            cout << "\nHasil pembagian: " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Pilihan tidak valid" << endl;
+            system("pause");
+            break;
+        }
+
+
+
+    } while (pilihan != 5); //!= adalah tidak sama dengan
+
+    return 0;
 }
